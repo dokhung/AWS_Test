ae8da9b5d6705735063d12aa95f37ecdf011a455
dokhung <94828988+dokhung@users.noreply.github.com>
2024-10-23 14:55:19 +0900

    test

--- src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/controller/menuController.java
+++ src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/controller/menuController.java
@@ -1,4 +1,13 @@
 package com.ohgiraffers.demoelasticbeanstalk.menu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class menuController {
+
+    @PostMapping
+    public void post(){
+        System.out.println("yes");
+    }
 }
